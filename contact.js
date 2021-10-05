f52ff9db5d5bc6d44129b01a69db5c2d91bed747
Oleksandr2891 <allavrenko@gmail.com>
2021-10-05 15:28:30 +0300

    task done

---
+++ contact.js
@@ -0,0 +1,97 @@
+const fs = require("fs").promises;
+
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "./db/contact.json");
+
+async function listContacts() {
+  const contacts = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+  const result = await contacts.map((item) => {
+    return {
+      Id: item.id,
+      Name: item.name,
+      Email: item.email,
+      Phone: item.phone,
+    };
+  });
+  return console.table(result);
+}
+
+// listContacts();
+
+async function getContactById(contactId) {
+  const contacts = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+  const result = await contacts.find((item) => {
+    if (item.id === contactId)
+      return {
+        Id: item.id,
+        Name: item.name,
+        Email: item.email,
+        Phone: item.phone,
+      };
+  });
+  return console.table(result);
+}
+
+// getContactById(5);
+
+async function removeContact(contactId) {
+  const contacts = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+
+  const result = await contacts.filter((item) => {
+    if (item.id !== contactId)
+      return {
+        Id: item.id,
+        Name: item.name,
+        Email: item.email,
+        Phone: item.phone,
+      };
+  });
+  fs.writeFile(contactsPath, JSON.stringify(result));
+  return console.table(result);
+}
+
+// removeContact(1);
+
+async function addContact(name, email, phone) {
+  const contacts = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+
+  const getId = () => {
+    const arrId = contacts.map((item) => +item.id);
+    const maxId = Math.max(...arrId);
+    console.log(`maxId`, maxId);
+    return maxId + 1;
+  };
+
+  const getContact = (name, email, phone) => {
+    return {
+      id: getId(),
+      name,
+      email,
+      phone,
+    };
+  };
+
+  const updateContacts = [...contacts, getContact(name, email, phone)];
+
+  fs.writeFile(contactsPath, JSON.stringify(updateContacts));
+
+  const result = await updateContacts.map((item) => {
+    return {
+      Id: item.id,
+      Name: item.name,
+      Email: item.email,
+      Phone: item.phone,
+    };
+  });
+  return console.table(result);
+}
+
+// addContact("Allen Raymond", "nulla.ante@vestibul.co.uk", "(992) 914-3792");
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
